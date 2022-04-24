d58f2a66fde5799dafba8c6808635e4fc166fc8a
Mladen Brankovic <root@brankovic.dev>
2022-04-24 13:38:46 +0200

    fix: remove dead reference

--- commands/tracker.js
+++ commands/tracker.js
@@ -46,7 +46,6 @@ module.exports = {
             enable: enableTrackerGlobally,
             disable: disableTrackerGlobally,
             scan: scanForPlayers,
-            info: displayTrackerInfo,
             allow: allowTracking,
             deny: denyTracking,
         };
