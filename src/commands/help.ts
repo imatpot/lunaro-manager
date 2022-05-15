31fa0ad23f4790a54b78c34152370768d4f53620
Mladen Brankovic <root@brankovic.dev>
2022-05-15 17:15:06 +0200

    interaction: add help

---
+++ src/commands/help.ts
@@ -0,0 +1,23 @@
+import { createCommand } from ':util/creators.ts';
+import { replyToInteraction } from ':util/interactions.ts';
+
+import { ApplicationCommandTypes } from 'discordeno';
+
+createCommand({
+    name: 'help',
+    description: '❓ Learn how to use Lunaro Manager',
+    type: ApplicationCommandTypes.ChatInput,
+
+    run: async (_, interaction) => {
+        const helpString = [
+            "Swazdo-lah, surah! I'm the Lunaro Manager, and my job is to help you with all things Lunaro.",
+            '',
+            'Type `/` and use the sidebar to explore my capabilites, or check out the *Usage* section on my GitHub page:',
+            'https://github.com/imatpot/lunaro-manager#usage',
+        ].join('\n');
+
+        await replyToInteraction(interaction, {
+            content: helpString,
+        });
+    },
+});
