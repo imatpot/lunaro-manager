e36be1207b6e9c73feeb441f0243dcf50d10aae2
Mladen Brankovic <root@brankovic.dev>
2022-08-04 16:35:30 +0200

    feature: add username parsing

---
+++ src/util/discord-user.ts
@@ -0,0 +1,35 @@
+/** Regex matching a Lunaro Revival Server username (formatted as `<username> [<platform>]`) */
+const usernamePattern = /([A-Za-z0-9_.-]{4,24})\s\[(PC|XB1|PS4|SWI)\]/;
+
+/** Represents a platform Warframe can be played on. */
+type Platform = 'PC' | 'XB1' | 'PS4' | 'SWI';
+
+/** A Lunaro Revival Server Discord user. */
+export class DiscordUser {
+    /** The username / in-game name. */
+    username: string;
+
+    /** The player's primary platform. */
+    platform: Platform;
+
+    constructor(username: string, platform: Platform) {
+        this.username = username;
+        this.platform = platform;
+    }
+
+    /**
+     * Parses a Lunaro Revival Server username .
+     *
+     * @param discordName the name to be parsed
+     * @returns the corresponding user or null if the name cannot be parsed
+     */
+    static parse = (discordName: string): DiscordUser | null => {
+        const matchGroups = discordName.match(usernamePattern);
+        const username = matchGroups?.[1];
+        const platform = matchGroups?.[2];
+
+        if (!username || !platform) return null;
+
+        return new DiscordUser(username, platform as Platform);
+    };
+}
